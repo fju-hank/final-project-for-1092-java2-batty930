17ceb11ff5d9e54543310fe049d2654ec335258f
batty930 <75302155+batty930@users.noreply.github.com>
2021-06-14 13:32:49 +0800

    FindOperation commit

--- src/main/java/com/fju/EquipmentSystem/operation/FindOperation.java
+++ src/main/java/com/fju/EquipmentSystem/operation/FindOperation.java
@@ -1,4 +1,22 @@
 package com.fju.EquipmentSystem.operation;
 
-public class FindOperation {
+import com.fju.EquipmentSystem.EquipmentList;
+import java.util.Scanner;
+
+public class FindOperation implements IOperation {
+    @Override
+    public void work(EquipmentList equipmentList) {
+        // 實現查詢某個裝備的資訊
+        System.out.println("查詢裝備");
+        // 讓使用者輸入裝備名, 來進行查詢
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("請輸入要查詢的裝備名: ");
+        String name = scanner.next();
+        // 迴圈遍歷裝備列表
+        for (int i = 0; i < equipmentList.getSize(); i++) {
+            if(equipmentList.getEquipment(i).getName().contains(name)) {
+                System.out.println(equipmentList.getEquipment(i));
+            }
+        }
+    }
 }
