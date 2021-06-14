f7a0df72f4944ec414d3797d5ee81169bf928b90
batty930 <75302155+batty930@users.noreply.github.com>
2021-06-14 13:09:12 +0800

    User Tester commit

--- src/main/java/com/fju/EquipmentSystem/Equipment.java
+++ src/main/java/com/fju/EquipmentSystem/Equipment.java
@@ -38,5 +38,5 @@ public class Equipment {
         public void setBorrowed(boolean borrowed) {
             isBorrowed = borrowed;
         }
-    }
 }
+
--- src/main/java/com/fju/EquipmentSystem/User.java
+++ src/main/java/com/fju/EquipmentSystem/User.java
@@ -1,4 +1,19 @@
 package com.fju.EquipmentSystem;
 
-public class User {
+import com.fju.EquipmentSystem.operation.IOperation;
+
+// 使用抽象方法User來存放兩Manager 和 Associator的資訊, 再由該類的子類來進一步的重寫這樣的方法
+
+abstract public class User {
+    protected String name;
+    protected IOperation[] operations;
+
+    public abstract int menu();
+
+    public void doOperation(int choice, EquipmentList equipmentList) {
+        IOperation operation = this.operations[choice - 1];
+        // 再呼叫 work 方法,執行引用對應的子類的方法
+        operation.work(equipmentList);
+    }
 }
+
