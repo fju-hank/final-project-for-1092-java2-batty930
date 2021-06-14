6e0be89e1e2c8f77c8aae169d4bc602dde4bb726
batty930 <75302155+batty930@users.noreply.github.com>
2021-06-14 13:35:03 +0800

    ExitOperation commit

--- src/main/java/com/fju/EquipmentSystem/operation/ExitOperation.java
+++ src/main/java/com/fju/EquipmentSystem/operation/ExitOperation.java
@@ -1,4 +1,11 @@
 package com.fju.EquipmentSystem.operation;
 
-public class ExitOperation {
+import com.fju.EquipmentSystem.EquipmentList;
+
+public class ExitOperation implements IOperation {
+    @Override
+    public void work(EquipmentList equipmentList) {
+        System.out.println("退出程式");
+        System.exit(0);
+    }
 }
